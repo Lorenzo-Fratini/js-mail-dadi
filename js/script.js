b1a5910b80608e7a7e5c785ae116c376e22531fd
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-03-04 16:53:23 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -1,40 +1,37 @@
 // Autenticazione email
-// var arr = [ 'demo@mail.com', 'my@mail.com', 'your@mail.com'];
-//
-// var enter = prompt('enter your email');
-//
-// var result;
-//
-// for (var i = 0; i < arr.length; i++){
-//
-//   if (enter == arr[i]){
-//     result = 'Autenticazione riuscita';
-//     break;
-//   } else{
-//     result = 'Autenticazione non riuscita';
-//   }
-// }
-//
-// document.getElementById('result').innerHTML =  result;
+var arr = [ 'demo@mail.com', 'my@mail.com', 'your@mail.com'];
 
+var enter = prompt('enter your email');
 
-// Gioco dei dadi
-var min = 1;
-var max = 6;
+var result = 'Autenticazione fallita';
 
-var rndUser = Math.floor(Math.random() * max) + min;
-var rndCom = Math.floor(Math.random() * max) + min;
+for (var i = 0; i < arr.length; i++){
 
-var result;
-
-if (rndUser > rndCom){
-   result="Hai vinto!";
-} else if (rndUser < rndCom){
-   result="Hai perso...";
-} else{
-   result="Hai pareggiato.";
+  if (enter == arr[i]){
+    result = 'Autenticazione riuscita';
+  }
 }
 
 document.getElementById('result').innerHTML =  result;
-document.getElementById('yourResult').innerHTML =  'Il tuo punteggio: ' + rndUser;
-document.getElementById('comResult').innerHTML =  'Il punteggio del tuo avversario: ' + rndCom;
+
+
+// Gioco dei dadi
+// var min = 1;
+// var max = 6;
+//
+// var rndUser = Math.floor(Math.random() * max) + min;
+// var rndCom = Math.floor(Math.random() * max) + min;
+//
+// var result;
+//
+// if (rndUser > rndCom){
+//    result="Hai vinto!";
+// } else if (rndUser < rndCom){
+//    result="Hai perso...";
+// } else{
+//    result="Hai pareggiato.";
+// }
+//
+// document.getElementById('result').innerHTML =  result;
+// document.getElementById('yourResult').innerHTML =  'Il tuo punteggio: ' + rndUser;
+// document.getElementById('comResult').innerHTML =  'Il punteggio del tuo avversario: ' + rndCom;
